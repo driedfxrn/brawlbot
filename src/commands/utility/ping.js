8bdec433ea87340a996ed181ceb43da656e441c5
fern <nasocancode@gmail.com>
2024-04-24 21:11:33 +0530

    Ping.js updated to show lantency

--- src/commands/utility/ping.js
+++ src/commands/utility/ping.js
@@ -3,8 +3,8 @@ const { SlashCommandBuilder } = require('discord.js')
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ping')
-        .setDescription('Replies with pong.'),
+        .setDescription('Replies with latency.'),
     async execute(interaction) {
-        await interaction.reply({ content: 'Pong! :D', ephemeral: true})
+        await interaction.reply({ content: `**${Date.now() - message.createdTimestamp}**ms.`, ephemeral: true})
     }
 }
